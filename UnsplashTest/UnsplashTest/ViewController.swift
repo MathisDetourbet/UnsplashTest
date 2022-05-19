42f4465002e4c5f69559a8122b40b86101af6771
Mathis Detourbet <mathis.detourbet@adevinta.com>
2022-05-19 11:02:15 +0200

    [Core] upload xcode initial project

---
+++ UnsplashTest/UnsplashTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UnsplashTest
+//
+//  Created by Mathis DETOURBET on 19/05/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
